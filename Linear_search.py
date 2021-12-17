5bb489fb9e6046cb63d5517560adc7a78d6ff1bd
Javier-rgb <javier.rgb@hotmail.com>
2021-12-16 20:51:10 -0700

    Linear search with counter

--- Linear_search.py
+++ Linear_search.py
@@ -2,12 +2,16 @@ import random
 
 def linear_search(list, value):
     found = False
+    i = 1
 
     for element in list:    # O(n)
         if element == value:
             found = True
             break
-        
+        else:
+            i = i + 1
+    
+    print(f"Number of tries: {i}")
     return found
 
 
@@ -15,7 +19,7 @@ if __name__ == '__main__':
     list_size = int(input("Enter the size of the list: "))
     value = int(input("Enter the value you want to check: "))
 
-    list = [random.randint(1, 100) for i in range(list_size)]
+    list = [random.randint(1, 100) for i in range(list_size - 1)]
 
     print(list)
     found = linear_search(list, value)
